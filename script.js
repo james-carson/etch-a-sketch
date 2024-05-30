89dd4e8f66ffe83eb2bc4c04b78577908306d37c
carsoninho <james.87.carson@gmail.com>
2024-05-30 12:07:36 +0200

    Create grid function
    Finally cracked it - needed to create the div inside the loop.

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 const gridContainer = document.querySelector(".grid_container");
+gridContainer.classList.add("gridContainer");
 
-const cell = document.createElement("div");
-cell.classList.add("cell");
-
-let size = 16;
-    rows = size;
-    columns = size;
-    cellSize = (640 / size) + "px";
-
-    cell.style.height = cellSize;
-    cell.style.width = cellSize;
-
-    console.log(cell.style.height);
-    console.log(cell.style.width);
+const size = prompt("What size is your grid going to be, mate?");
 
 function createGrid() {
-    for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < columns; c++) {
+    if (size > 0 && size <= 100) {
+    for (let r = 0; r < size; r++) {
+        for (let c = 0; c < size; c++) {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            let cellSize = ((640 / size) - 2) + "px";
+            cell.style.height = cellSize;
+            cell.style.width = cellSize;
             gridContainer.appendChild(cell);
-        }
+        }}
+    } else if (size > 100){
+        alert("You can't have over 100 lad.")
+    } else {
+        alert("You need to enter a number between 1 and 100")
     }
 }
 
